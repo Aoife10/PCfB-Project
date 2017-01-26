aad842e2940e6fcbc23836b391615b6e9282297c
annabelliard <annabelliard@gmail.com>
2017-01-26 23:04:31 +0100

    Update and rename MainProject.sh to Threats.sh

--- MainProject.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-newfile.py
-module load R/3.3.1-foss-2016a
-
-Rscript Seagrassproject.R
-mv Rplots.pdf Na1.pdf
---
+++ Threats.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+Threats.py
+module load R/3.3.1-foss-2016a
+Rscript Threats.R
